ce0bb19b65b577c62c49c573399a64d9ca6cdbf7
AmandaKly <amandaklly0@gmail.com>
2023-01-25 11:53:54 -0300

    adicionando um guia às rotas do backend

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,28 +1,48 @@
 import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
 import './App.css';
+import api from "./services/api";
 import Home from "./pages/Home";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
 import PontosDeColeta from "./pages/PontosDeColeta";
 import ColetaSeletiva from "./pages/ColetaSeletiva";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-     <BrowserRouter>
+        <div className="App">
+         <BrowserRouter>
+        
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/Pontos de coleta" element={<PontosDeColeta/>} />
+            <Route path="/Coleta seletiva" element={<ColetaSeletiva />} />
     
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Cadastro" element={<Cadastro />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Pontos de coleta" element={<PontosDeColeta/>} />
-        <Route path="/Coleta seletiva" element={<ColetaSeletiva />} />
-
-
-      </Routes>
-    </BrowserRouter>
-    </div>
-  );
+    
+          </Routes>
+        </BrowserRouter>
+        </div>
+      );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//      <BrowserRouter>
+    
+//     <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/Cadastro" element={<Cadastro />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/Pontos de coleta" element={<PontosDeColeta/>} />
+//         <Route path="/Coleta seletiva" element={<ColetaSeletiva />} />
 
-export default App;
+
+//       </Routes>
+//     </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
--- start/routes.ts
+++ start/routes.ts
@@ -19,70 +19,93 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
-// Route.get('/', 'UserController.index').as('index').middleware('auth')
 
 Route.group(() => {
    
         
     Route.group(() => {
+        // Essa rota serve para listar toodos os usuários cadastrados.
     Route.get('/', 'UsersController.index')
         .as('usuarios.index')
 
-    // Route.get('/:id', 'UsersController.show')
-    //     .where('id', /^[0-9]+$/)
-    //     .as('usuarios.show')
+        // Essa rota serve para cadastrar um usuário.
     Route.post('/', 'UsersController.store')
         .as('usuarios.store')
 
+        // Essa rota serve para atualizar dados de um usuário cadastrado.
     Route.patch('/:id', 'UsersController.update')
         .where('id', /^[0-9]+$/)
         .as('usuarios.update').middleware('auth')
     
+        // Essa rota serve para deletar um usuário.
         Route.delete('/:id', 'UsersController.destroy')
         .where('id', /^[0-9]+$/)
         .as('usuarios.destroy').middleware('auth')
 
+        // Essa rota serve para criar um ponto de coleta ('/usuario/pontos')
     Route.post('/:id/pontos', 'PontosController.store')
         .where('id', /^[0-9]+$/)
         .as('pontos.store').middleware('auth')
         
+        // ATENÇÃO! TODAS AS ROTAS PARA USUÁRIO DEVEM INICIAR ANTES COM O PREFIXO /usuario
     }).prefix('/usuario')
 
 
     Route.group(() => {
+        // Essa rota serve para fazer login.
     Route.post('/login','AuthController.login')
         .as('auth.login')
+        // Essa rota serve para sair da conta.
     Route.post('/logout','AuthController.logout')
         .as('auth.logout').middleware('auth')
     }).prefix('/auth')
     
     Route.group(() =>{
+        // Essa rota serve para listar todos os pontos de coletas.
     Route.get('/', 'PontosController.index')
        .as('pontos.index')
+
+       // Essa rota serve para listar tum ponto de coleta específico.
     Route.get('/:id', 'PontosController.show')
        .where('id', /^[0-9]+$/)
        .as('pontos.show')
+
+       // Essa rota serve para deletar um ponto de coleta.
     Route.delete('/:id', 'PontosController.destroy')
        .where('id', /^[0-9]+$/)
        .as('pontos.destroy').middleware('auth')
+
+       // Essa rota serve para atualizar um ponto de coleta.
     Route.patch('/:id', 'PontosController.update')
         .where('id', /^[0-9]+$/)
         .as('pontos.update').middleware('auth')
 
+        // Essa rota serve para criar materiais para um de coleta. (use a rota '/pontos/materiais')
     Route.post('/:id/materiais', 'MateriaisController.store')
         .where('id', /^[0-9]+$/)
         .as('materiais.store').middleware('auth')
+
+        // ATENÇÃO! TODAS AS ROTAS PARA PONTOS DEVEM INICIAR ANTES COM O PREFIXO /pontos
     }).prefix('/pontos')
 
     Route.group(() =>{
+        // Essa rota serve para listar todos os materiais.
         Route.get('/', 'MateriaisController.index')
+
+          // Essa rota serve para listar todos os pontos pertencentes a um tipo espefíco de material.
         Route.post('/listar', 'MateriaisController.findbycategoria')
+
+        // Essa rota serve para deletar um material.
         Route.delete('/:id', 'MateriaisController.destroy')
            .where('id', /^[0-9]+$/)
            .as('materiais.destroy').middleware('auth')
+
+        // Essa rota serve para editar um material.
         Route.patch('/:id', 'MateriaisController.update')
             .where('id', /^[0-9]+$/)
             .as('materiais.update').middleware('auth')
+
+            // ATENÇÃO! TODAS AS ROTAS PARA MATERIAIS DEVEM INICIAR ANTES COM O PREFIXO /materiais
         }).prefix('/materiais')
 
 }).prefix('/api')
@@ -90,9 +113,13 @@ Route.group(() => {
 
 
 
-Route.group(() =>{
+// Route.group(() =>{
     
-}).prefix('/postagem')
-    .middleware('auth')
-    .where('id', /^[0-9]+$/)
-    .where('idComment', /^[0-9]+$/)
+// }).prefix('/postagem')
+//     .middleware('auth')
+//     .where('id', /^[0-9]+$/)
+//     .where('idComment', /^[0-9]+$/)
+
+    // Route.get('/:id', 'UsersController.show')
+    //     .where('id', /^[0-9]+$/)
+    //     .as('usuarios.show')
